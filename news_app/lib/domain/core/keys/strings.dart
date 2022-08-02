cc13b0dceca16dd3ecafca20114652fa93e0ce8b
shibilstark <shibilhassankmangode@gmail.com>
2022-08-02 19:03:43 +0530

    ready to begin

---
+++ news_app/lib/domain/core/keys/strings.dart
@@ -0,0 +1,26 @@
+class PersonalKeys {
+  static const key = "cd2203707947413b8288ef6e7431c3fd";
+  static const baseUrl = "https://newsapi.org/v2";
+  static const topHeadlines = "/v2/top-headlines";
+  static const eg =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cd2203707947413b8288ef6e7431c3fd";
+}
+
+class ApiContry {
+  static const ae = "ae";
+  static const us = "us";
+  static const india = "in";
+  static const ua = "ua";
+  static const ru = "ru";
+  static const ca = "ca";
+}
+
+class ApiCategory {
+  static const business = "business";
+  static const entertainment = "entertainment";
+  static const general = "general";
+  static const health = "health";
+  static const science = "science";
+  static const sports = "sports";
+  static const technology = "technology";
+}
